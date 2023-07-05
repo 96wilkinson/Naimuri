ccaab33cc1c437bc8139c7787ab8f2b8e8820593
Anthony Wilkinson <96wilkinson@sky.com>
2023-07-05 19:15:34 +0100

    created some try catches to reduce any further errors

--- components/reduceDictByNumber.js
+++ components/reduceDictByNumber.js
@@ -1,9 +1,13 @@
 module.exports = async function reduceDict(Number, Arr) {
-    let returnArr = []
-    for (let i = 0; i < Arr.length; i++) {
-        if (await Arr[i].length == Number) {
-            returnArr.push(Arr[i])
+    try {
+        let returnArr = []
+        for (let i = 0; i < Arr.length; i++) {
+            if (await Arr[i].length == Number) {
+                returnArr.push(Arr[i])
+            }
         }
+        return returnArr;
+    } catch (error) {
+        return error
     }
-    return returnArr;
 }
