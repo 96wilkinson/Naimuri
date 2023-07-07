a2074d5806c77dcfaa816e639ec81684ab9e1767
Anthony Wilkinson <96wilkinson@sky.com>
2023-07-07 14:52:11 +0100

    tried getting words with only 1 unique letter but rose goes against this having both r and s

--- components/reduceBy/reduceDictByUniqueLetters.js
+++ components/reduceBy/reduceDictByUniqueLetters.js
@@ -5,13 +5,20 @@ module.exports = async function reduceDictByUniqueLetters(arrayOfUniqueLetters, 
         let arrayOfWordsContainingUniqueLetters = []
         for (let i = 0; i < remainingWordsArray.length; i++) {
             tempValForWord = remainingWordsArray[i].split('')
+            let truthVar = []
             for (let j = 0; j < tempValForWord.length; j++) {
-
-                //console.log(tempValForWord[j],arrayOfUniqueLetters)
+                // create array of if it contains unique letter add to array true. if more  than 1 true dont add it
+                console.log(tempValForWord[j], arrayOfUniqueLetters)
                 if (arrayOfUniqueLetters.includes(tempValForWord[j])) {
-                    arrayOfWordsContainingUniqueLetters.push(remainingWordsArray[i])
-                } else {continue}
+                    truthVar.push(true)
+                } else {
+                    truthVar.push(false)
+                }
             }
+            if (truthVar.filter(x => x == true).length == 1) {
+                arrayOfWordsContainingUniqueLetters.push(remainingWordsArray[i])
+            } else { continue }
+
         }
         return await arrayOfWordsContainingUniqueLetters
     } catch (error) {
